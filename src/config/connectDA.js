da0a712754fca870cb666d9dc73bcd64506780b0
hoangEgtechgroup <140359385+hoangEgtechgroup@users.noreply.github.com>
2023-07-27 16:57:40 +0700

    27/07

--- src/config/connectDA.js
+++ src/config/connectDA.js
@@ -1,12 +1,12 @@
 var mysql = require('mysql');
-// require('dotenv').config()
+require('dotenv').config()
 
 // CREATE A CONNECTION
 const pool = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: "",
-    database: 'admin_testappwall'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 });
 
 // CONNECTION
